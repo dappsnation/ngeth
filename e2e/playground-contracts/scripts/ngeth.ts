88fcde84b2e0a54485e29c92bc6c44240a3083dd
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2022-03-04 10:29:32 +0100

    put hardhat in ngeth

---
+++ e2e/playground-contracts/scripts/ngeth.ts
@@ -0,0 +1,54 @@
+import { task } from 'hardhat/config';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { join } from 'path';
+import { generate, getContractNames } from './ng-generate';
+import { promises as fs } from 'fs';
+
+task('ngeth:test', 'run test with jest', async (taskArguments: any, hre: any) => {
+  const { runCLI } = await import("jest");
+  const root = hre.config.paths.root;
+  const jestConfig: any = { watch: taskArguments.watch ?? false };
+  const failures = await runCLI(jestConfig, [join(root, "/jest.config.js")]);
+  process.exitCode = failures.results.success ? 0 : 1;
+});
+
+task('ngeth:build', '', async (taskArguments: any, hre: HardhatRuntimeEnvironment) => {
+  return hre.run('compile', taskArguments);
+});
+
+task('ngeth:serve', '',  async (taskArguments: any, hre: HardhatRuntimeEnvironment) => {
+  await hre.run('ngeth:build', taskArguments);
+  return hre.run('node', taskArguments);
+});
+
+task('node:server-ready', 'Run once the node is ready',
+  async (taskArguments: any, hre: any, runSuper: any) => {
+    await runSuper();
+    await generate(hre);
+    const root = hre.config.paths.root;
+    const setEnv = (addresses: Record<string, string>) => {
+      const envPath = join(root, 'environments/environment.ts');
+      return fs.writeFile(envPath, `export default ${JSON.stringify({ addresses })};`);
+    } 
+
+    // Deploy all contract and update environment
+    const names = await getContractNames(hre);
+    
+    const addresses = {};
+    const params = {
+      BaseERC1155: [''],
+    };
+
+    const deploy = async (name: string) => {
+      const [_, contractName] = name.split(':');
+      const Contract = await hre.ethers.getContractFactory(name);
+      const contract = (contractName in params)
+        ? await Contract.deploy(...params[contractName])
+        : await Contract.deploy();
+      await contract.deployed();
+      addresses[contractName] = contract.address;
+    };
+    await Promise.all(names.map(deploy));
+    await setEnv(addresses);
+  }
+);
