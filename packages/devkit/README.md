7b24e6820aa10b2fea48f4f4a7f2b0392b8a2469
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2022-03-09 15:36:03 +0100

    add devkit library

---
+++ packages/devkit/README.md
@@ -0,0 +1,7 @@
+# devkit
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build devkit` to build the library.
--- packages/ipfs/schematics/add/index.ts
+++ packages/ipfs/schematics/add/index.ts
@@ -1,9 +1,6 @@
-import { updateJson, Tree, convertNxGenerator, names, getWorkspaceLayout, readWorkspaceConfiguration, addDependenciesToPackageJson, addProjectConfiguration, updateProjectConfiguration, getWorkspacePath, readJson, joinPathFragments, Workspace, ProjectConfiguration, TargetConfiguration } from '@nrwl/devkit';
+import { Tree, convertNxGenerator, names, getWorkspaceLayout, readWorkspaceConfiguration, addDependenciesToPackageJson, addProjectConfiguration, updateProjectConfiguration, getWorkspacePath, readJson, joinPathFragments, Workspace, ProjectConfiguration, TargetConfiguration } from '@nrwl/devkit';
+import { setProjectTargets, updateTsConfig } from '@ngeth/devkit';
 
-type WorkspaceOrProject = Workspace | ProjectConfiguration;
-function isProjectConfig(workspace: WorkspaceOrProject): workspace is ProjectConfiguration {
-  return 'projectType' in workspace;
-}
 
 interface BaseOptions {
   project?: string;
@@ -25,60 +22,6 @@ function getOptions(tree: Tree, baseOptions: BaseOptions): Options {
   };
 }
 
-export function updateTsConfig(tree: Tree, options: { projectRoot: string }, updates: Record<string, any>) {
-  const tsConfig = `${options.projectRoot}/tsconfig.json`;
-
-  updateJson(tree, tsConfig, json => {
-    if (!json.compilerOptions) json.compilerOptions = {};
-    for (const [key, value] of Object.entries(updates)) {
-      json.compilerOptions[key] = value;
-    }
-    return json;
-  })
-}
-
-export function readRawWorkspaceJson(tree: Tree) {
-  const path = getWorkspacePath(tree);
-  if (!path) throw new Error('No file angular.json or workspace.json found');
-  if (!tree.exists(path)) throw new Error(`No file ${path} found`);
-  return readJson<{ projects: Record<string, any> }>(tree, path);
-}
-
-export function getProjectFileLocation(tree: Tree, project: string): string | null {
-  const rawWorkspace = readRawWorkspaceJson(tree);
-  const projectConfig = rawWorkspace.projects?.[project];
-  return typeof projectConfig === 'string'
-    ? joinPathFragments(projectConfig, 'project.json')
-    : null;
-}
-
-export function getProjectConfig(workspace: WorkspaceOrProject, project: string): ProjectConfiguration {
-  if (isProjectConfig(workspace)) return workspace;
-  return workspace.projects[project];
-}
-
-export function updateProjectConfig(tree: Tree, project: string, cb: (config: ProjectConfiguration) => void) {
-  const projectFile = getProjectFileLocation(tree, project);
-  if (!projectFile) throw new Error('Could not find project file');
-  return updateJson(tree, projectFile, (file: WorkspaceOrProject) => {
-    const config = getProjectConfig(file, project);
-    if (!config) return file;
-    cb(config);
-    return file;
-  });
-}
-
-export function setProjectTargets(tree: Tree, project: string, targets: Record<string, TargetConfiguration>) {
-  return updateProjectConfig(tree, project, config => {
-    if (!config.targets) {
-      config.targets = targets;
-    } else {
-      for (const [key, value] of Object.entries(targets)) {
-        config.targets[key] = value;
-      }
-    }
-  })
-}
 
 export async function nxGenerator(tree: Tree, baseOptions: BaseOptions) {
   const options = getOptions(tree, baseOptions);
