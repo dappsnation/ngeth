7cfa71d26f57f4d3fa00a8e1f8379ef602b5fc60
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2022-04-25 18:53:10 +0200

    improve chain manager

--- packages/ethers/src/lib/chain/service.ts
+++ packages/ethers/src/lib/chain/service.ts
@@ -1,8 +1,8 @@
 import { Inject, Injectable, InjectionToken } from "@angular/core";
 import { MetaMask } from "../metamask";
-import { ChainIcon, Chain, SupportedChains } from "./types";
+import { ChainIcon, Chain, SupportedChains, ChainId } from "./types";
 import { explore, getChain, getChainIcons, defaultCustomChains, toChainId } from "./utils";
-import { switchMap } from "rxjs";
+import { defer, switchMap } from "rxjs";
 
 export const CUSTOM_CHAINS = new InjectionToken<Record<string, Chain>>('Custom Chains to use instead of https://github.com/ethereum-lists/chains', {
   providedIn: 'root',
@@ -19,16 +19,16 @@ export class ChainManager {
   private chains: Record<string, Chain> = {};
   private icons: Record<string, ChainIcon> = {};
 
-  chain$ = this.metamask.chainId$.pipe(
+  chain$ = defer(() => this.metamask.chainId$.pipe(
     switchMap(chainId => this.getChain(chainId)),
-  );
+  ));
 
   constructor(
     private metamask: MetaMask,
     @Inject(CUSTOM_CHAINS) private customChains: Record<string, Chain>
   ) {}
 
-  async getChain(chainId: string | number = this.metamask.chainId): Promise<Chain> {
+  async getChain(chainId: ChainId = this.metamask.chainId): Promise<Chain> {
     const id = toChainId(chainId);
     if (id in this.customChains) return this.customChains[id];
     if (!this.chains[id]) {
@@ -45,8 +45,8 @@ export class ChainManager {
     return this.icons[key];
   }
 
-  async explore(search: string) {
-    const chain = await this.getChain(this.metamask.chainId);
+  async explore(search: string, chainId: ChainId = this.metamask.chainId) {
+    const chain = await this.getChain(chainId);
     return explore(chain, search);
   }
 }
