8dc9e147a1d743cbbd5a2ff07ba3edc293be94f6
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2022-05-19 11:16:31 +0200

    create hardhat-gui

---
+++ e2e/hardhat-gui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'eth-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'hardhat-gui';
+}
