aea3c1bf763496f6a54f5eea5c236139e5a3a8df
Camille Hautois <camille.hautois@gmail.com>
2022-06-07 17:56:39 +0200

    correction balanceHistory: delete ??0x00

--- e2e/explorer/api/src/app/etherscan/account.ts
+++ e2e/explorer/api/src/app/etherscan/account.ts
@@ -85,8 +85,8 @@ export function getMinedBlocks(params: GetParams<BlockMined>) {
 export function balanceHistory(params: GetParams<BalanceHistory>) {
   const { address, blockno } = params;
   if (!address || ! blockno) throw new Error('Error! Missing or invalid Action name');
-  
-  const balance = states[blockno]?.balances[address] ?? '0x00';
+
+  const balance = states[blockno]?.balances[address];
   if (!balance) return '0';
   return balance.toString().replace('0x', '');
 }
