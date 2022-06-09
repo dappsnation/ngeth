6835c7b5918f1e9a4d5b89ec3d4d8a0ca84fb28c
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2022-06-09 14:05:55 +0200

    improve error handling in abi

--- e2e/explorer/app/src/app/pages/contract/view/abi/abi.component.ts
+++ e2e/explorer/app/src/app/pages/contract/view/abi/abi.component.ts
@@ -1,5 +1,6 @@
 import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { ViewComponent } from '../view.component';
+import { FormArray } from '@angular/forms';
 import { AbiFormFunction, formABI } from './utils';
 import { map } from 'rxjs/operators';
 import { WalletManager } from '../../../../wallet';
@@ -24,31 +25,36 @@ export class AbiComponent {
     private cdr: ChangeDetectorRef,
   ) { }
 
+  private onError(form: FormArray, error: Error) {
+    const err = error.message.length < 300 ? error.message : 'An error occured. Check your console';
+    form.addValidators(() => ({ err }));
+    form.updateValueAndValidity();
+    console.error(err);
+  }
+
   async callRead(read: AbiFormFunction) {
     if (!read.name) return;
-    if (read.form.invalid) return read.form.markAllAsTouched();
     const inputs = read.form.value;
     try {
       read.result = await this.shell.contract?.callStatic[read.name](...inputs);
+      read.form.clearValidators();
       read.form.reset();
-    } catch(err) {
-      read.form.addValidators(() => ({ err }));
-      read.form.updateValueAndValidity();
+    } catch(error) {
+      this.onError(read.form, error as Error);
     }
     this.cdr.markForCheck();
   }
 
   async callWrite(write: AbiFormFunction) {
     if (!write.name) return;
-    if (write.form.invalid) return write.form.markAllAsTouched();
     const inputs = write.form.value;
     try {
       const tx = await this.shell.contract?.functions[write.name](...inputs);
       await tx.wait();
+      write.form.clearValidators();
       write.form.reset();
-    } catch(err) {
-      write.form.addValidators(() => ({ err }));
-      write.form.updateValueAndValidity();
+    } catch(error) {
+      this.onError(write.form, error as Error);
     }
     this.cdr.markForCheck();
   }
