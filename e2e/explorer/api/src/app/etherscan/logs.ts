0cbd29476dceeccdbb0127d1b505447566f5ae67
Camille Hautois <camille.hautois@gmail.com>
2022-06-13 15:52:20 +0200

    update getLogs : add operators to topics

--- e2e/explorer/api/src/app/etherscan/logs.ts
+++ e2e/explorer/api/src/app/etherscan/logs.ts
@@ -2,20 +2,41 @@ import { GetParams, Logs } from "./types";
 import { store } from '../store';
 
 export function getLogs(params: GetParams<Logs>) {
-  const {
-    fromBlock, toBlock, address, topic0, topic1, topic2, topic3,
-    topic0_1_opr, topic0_2_opr, topic0_3_opr, topic1_2_opr, topic1_3_opr, topic2_3_opr
-  } = params;
+  const { fromBlock, toBlock, address } = params;
   if (!fromBlock || !toBlock || !address) throw new Error('Error! Missing or invalid Action name');
+
+  let filter: {from: number; to: number; operator: 'or' | 'and' };
+  from:
+  for (let i = 0; i<3; i++) {
+    for (let j = i +1; j<4; j++) {
+      const operator = params[`topic${i}_${j}_opr` as any];
+      if(operator) {
+        filter = { from: i, to: j, operator};
+        break from;
+      }
+    }
+  };
+  if (!filter) {
+    filter = { from: 0, to: 3, operator: "and" };
+  }
+
+  // todo check if toBlock/fromBlock is latest mutate them in block.lenght-1
 
   return Object.values(store.logs).flat().filter(log => {
     if(log.address !== address) return false;
     if(log.blockNumber <= fromBlock || log.blockNumber >= toBlock ) return false;
-    else if (fromBlock === "latest" || toBlock === "latest") return true;
-    if(log.topics.filter(topic => {
-      if(topic !== topic0 || topic !== topic1 || topic !== topic2 || topic !== topic3) return false;
-      return true; 
-      }))
-    return true;
+    if (filter.operator === "and") {
+      for (let i = filter.from; i <= filter.to; i++) {
+        if (!params[`topic${i}`]) continue;
+        if(params[`topic${i}` as any] !== log.topics[i]) return false;
+      }
+      return true;
+    } else if (filter.operator === "or") {
+      for (let i = filter.from; i <= filter.to; i++) {
+        if (params[`topic${i}` as any] === log.topics[i]) return true;
+      }
+      return false;
+    }
+  throw new Error('Operator should be either "and" or "or"');
   })
 }
