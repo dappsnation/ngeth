63d70ac546cd816fe7b4e76e613404a87d62d1a6
Camille Hautois <camille.hautois@gmail.com>
2022-07-04 18:16:54 +0200

    update etherscan.ts function: results in string are convert to their type

--- packages/etherscan/src/lib/etherscan.ts
+++ packages/etherscan/src/lib/etherscan.ts
@@ -63,31 +63,38 @@ function initEtherscan(apiKey: string, baseUrl: string) {
 function formatDate(date: Date) {
   return date.toISOString().split('T')[0];
 }
-/** Transform a bigNumber into a string */
-function formatBigNumber(bigNumber: BigNumber) {
-  return bigNumber.toString();
+
+/** Transform a string into a bigNumber */
+function toBigNumber(value: string): BigNumber {
+  return BigNumber.from(value);
 }
-/** Transform a boolean into string; "1": true, "0": false*/
-function formatBoolean(bool: boolean) {
- if(bool === true) return "1"
- return "0" 
+/** Transform a string into boolean */
+function toBoolean(bool: '0' | '1'): boolean {
+  return bool === '0' ? false : true;
 }
-/** Transform a number into a string */
-function formatNumber(num: number) {
-  return num.toString();
+/** Transform a string into a number */
+function toNumber(value: string): number {
+  return parseInt(value);
 }
 
 //////////////
 // ACCOUNTS //
 //////////////
 
-function balance(call: Etherscan, address: string, tag: Tag) {
-  return call<string>({ module: 'account', action: 'balance', address, tag });
+async function balance(call: Etherscan, address: string, tag: Tag) {
+  const res = await call<string>({ module: 'account', action: 'balance', address, tag });
+  return toBigNumber(res);
 }
 
-function balanceMulti(call: Etherscan, addresses: string[], tag: Tag) {
+async function balanceMulti(call: Etherscan, addresses: string[], tag: Tag) {
   const address = addresses.join(',');
-  return call<BalanceMultiResponse[]>({ module: 'account', action: 'balancemulti', address, tag });
+  const res = await call<BalanceMultiResponse[]>({ module: 'account', action: 'balancemulti', address, tag });
+  for (let i =0; i<res.length; i++) {
+    return {
+      account: res[i].account,
+      balance: toBigNumber(res[i].balance),
+    }
+  }
 }
 
 function txList(call: Etherscan, address: string, params: Optional<TxListRequest> = {}) {
@@ -131,13 +138,21 @@ function token1155Tx(call: Etherscan, contractaddress: string, params: Optional<
   return call<TransactionResponse[]>({ module: 'account', action: 'token1155tx', contractaddress, ...params });
 }
 
-function getMinedBlocks(call: Etherscan, address: string, params: Optional<MinedBlockRequest>) {
-  return call<MinedBlockResponse[]>({ module: 'account', action: 'getminedblocks', address, ...params });
+async function getMinedBlocks(call: Etherscan, address: string, params: Optional<MinedBlockRequest>) {
+  const res = await call<MinedBlockResponse[]>({ module: 'account', action: 'getminedblocks', address, ...params });
+  for (let i =0; i<res.length; i++) {
+    return {
+      blockNumber: toNumber(res[i].blockNumber),
+      timeStamp: res[i].timeStamp,
+      blockReward: toBigNumber(res[i].blockReward)
+    }
+  }
 }
 
 //block no might be optionnal but no testing available since it requires API pro
-function balanceHistory(call: Etherscan, address: string, blockno: number) {
-  return call<string>({ module: 'account', action: 'balancehistory', address, blockno });
+async function balanceHistory(call: Etherscan, address: string, blockno: number) {
+  const res = await call<string>({ module: 'account', action: 'balancehistory', address, blockno });
+  return toBigNumber(res)
 }
 
 ///////////////
@@ -163,82 +178,141 @@ async function getSourceCode(call: Etherscan, address: string) {
 // TRANSACTIONS //
 //////////////////
 
-function getStatus(call: Etherscan, txhash: string) {
-  return call<ExecutionStatusResult>({ module: 'transaction', action: 'getstatus', txhash });
+async function getStatus(call: Etherscan, txhash: string) {
+  const res = await call<ExecutionStatusResult>({ module: 'transaction', action: 'getstatus', txhash });
+  return {
+    isError: toBoolean(res.isError),
+    errDescription: res.errDescription
+  }
 }
 
-function getTxReceiptStatus(call: Etherscan, txhash: string) {
-  return call<StatusResult>({ module: 'transaction', action: 'gettxreceiptstatus', txhash });
+async function getTxReceiptStatus(call: Etherscan, txhash: string) {
+  const res = await call<StatusResult>({ module: 'transaction', action: 'gettxreceiptstatus', txhash });
+  return {
+    status: toBoolean(res.status),
+  }
 }
 
 ////////////
 // BLOCKS //
 ////////////
 
-function getBlockReward(call: Etherscan, blockno: number) {
-  return call<BlockRewardResponse>({ module: 'block', action: 'getblockreward', blockno });
+async function getBlockReward(call: Etherscan, blockno: number) {
+  const res = await call<BlockRewardResponse>({ module: 'block', action: 'getblockreward', blockno });
+  return {
+    blockNumber: toNumber(res.blockNumber),
+    timeStamp: res.timeStamp,
+    blockMiner: res.blockMiner,
+    blockReward: toBigNumber(res.blockReward),
+    uncles: res.uncles,
+    uncleInclusionReward: toNumber(res.uncleInclusionReward)
+  }
 }
 
-function getBlockCountdown(call: Etherscan, blockno: number) {
-  return call<BlockCountdownResponse>({ module: 'block', action: 'getblockcountdown', blockno });
+async function getBlockCountdown(call: Etherscan, blockno: number) {
+  const res = await call<BlockCountdownResponse>({ module: 'block', action: 'getblockcountdown', blockno });
+  return {
+    CurrentBlock: toNumber(res.CurrentBlock),
+    CountdownBlock: toNumber(res.CountdownBlock),
+    RemainingBlock: toNumber(res.RemainingBlock),
+    EstimateTimeInSec: toNumber(res.EstimateTimeInSec),
+  }
 }
 
-function getBlocknoByTime(call: Etherscan, timestamp: number, closest: Closest) {
-  return call<string>({ module: 'block', action: 'getblocknobytime', timestamp, closest });
+async function getBlocknoByTime(call: Etherscan, timestamp: number, closest: Closest) {
+  const res = await call<string>({ module: 'block', action: 'getblocknobytime', timestamp, closest }); 
+  return toNumber(res);
 }
 
 //////////////////////////
 // BLOCKS: STATS MODULE //
 //////////////////////////
 
-function dailyAvgBlockSize(call: Etherscan, startDate: Date, endDate: Date, params: Optional<DailyAvgBlocksizeRequest>) {
-  return call<DailyAvgBlocksizeResponse[]>({
+async function dailyAvgBlockSize(call: Etherscan, startDate: Date, endDate: Date, params: Optional<DailyAvgBlocksizeRequest>) {
+  const res = await call<DailyAvgBlocksizeResponse[]>({
     module: 'stats',
     action: 'dailyavgblocksize',
     startdate : formatDate(startDate),
     enddate: formatDate(endDate),
     ...params
   });
+  for (let i = 0; i< res.length; i++) {
+    return {
+      URCDate: res[i].UTCDate,
+      unixTimeStamp: res[i].unixTimeStamp,
+      blockSize_bytes: toNumber(res[i].blockSize_bytes)
+    }
+  }
 }
 
-function dailyBlkCount(call: Etherscan, startDate: Date, endDate: Date, params: Optional<DailyBlockCountAndRewardRequest>) {
-  return call<DailyBlockCountAndRewardResponse[]>({
+async function dailyBlkCount(call: Etherscan, startDate: Date, endDate: Date, params: Optional<DailyBlockCountAndRewardRequest>) {
+  const res = await call<DailyBlockCountAndRewardResponse[]>({
     module: 'stats',
     action: 'dailyblkcount',
     startdate : formatDate(startDate),
     enddate: formatDate(endDate),
     ...params
   });
+  for(let i =0; i<res.length; i++) {
+    return {
+      UTCDate: res[i].UTCDate,
+      unixTimeStamp: res[i].unixTimeStamp,
+      blockCount: toNumber(res[i].blockCount),
+      blockRewards_Eth: toBigNumber(res[i].blockRewards_Eth)
+    }
+  } 
 } 
 
-function dailyBlockRewards(call: Etherscan, startDate: Date, endDate: Date, params: Optional<DailyBlockRewardRequest>) {
-  return call<DailyBlockRewardResponse[]>({
+async function dailyBlockRewards(call: Etherscan, startDate: Date, endDate: Date, params: Optional<DailyBlockRewardRequest>) {
+  const res = await call<DailyBlockRewardResponse[]>({
     module: 'stats',
     action: 'dailyblockrewards',
     startdate : formatDate(startDate),
     enddate: formatDate(endDate),
     ...params
-  });  
+  }); 
+  for (let i = 0; i< res.length; i++) {
+    return {
+      URCDate: res[i].UTCDate,
+      unixTimeStamp: res[i].unixTimeStamp,
+      blockRewards_Eth: toBigNumber(res[i].blockRewards_Eth)
+    }
+  } 
 }
 
-function dailyBlockTime(call: Etherscan, startDate: Date, endDate: Date, params: Optional<DailyBlockTimeRequest>) {
-  return call<DailyBlockTimeResponse[]>({
+async function dailyBlockTime(call: Etherscan, startDate: Date, endDate: Date, params: Optional<DailyBlockTimeRequest>) {
+  const res = await call<DailyBlockTimeResponse[]>({
     module: 'stats',
     action: 'dailyavgblocktime',
     startdate : formatDate(startDate),
     enddate: formatDate(endDate),
     ...params
   });
+  for (let i = 0; i< res.length; i++) {
+    return {
+      URCDate: res[i].UTCDate,
+      unixTimeStamp: res[i].unixTimeStamp,
+      blockTime_sec: res[i].blockTime_sec
+    }
+  } 
 }
 
-function dailyUncleBlkCount(call: Etherscan, startDate: Date, endDate: Date, params: Optional<DailyUncleBlockCountRequest>) {
-  return call<DailyUncleBlockCountReponse[]>({
+async function dailyUncleBlkCount(call: Etherscan, startDate: Date, endDate: Date, params: Optional<DailyUncleBlockCountRequest>) {
+  const res = await call<DailyUncleBlockCountReponse[]>({
     module: 'stats',
     action: 'dailyuncleblkcount',
     startdate : formatDate(startDate),
     enddate: formatDate(endDate),
     ...params
   });
+  for(let i =0; i<res.length; i++) {
+    return {
+      UTCDate: res[i].UTCDate,
+      unixTimeStamp: res[i].unixTimeStamp,
+      uncleBlockCount: toNumber(res[i].uncleBlockCount),
+      uncleBlockRewards_Eth: toBigNumber(res[i].uncleBlockRewards_Eth)
+    }
+  } 
 }
 
 //////////
@@ -253,11 +327,11 @@ function getLogs(call: Etherscan, address: string, params: Optional<LogsRequest>
 // PROXY //
 ///////////
 
-function ethBlockNumber(call: Etherscan) {
-  return call<string>({ module: 'proxy', action: 'eth_getBlockByNumber' })
+async function blockNumber(call: Etherscan) {
+  return await call<string>({ module: 'proxy', action: 'eth_blockNumber' });
 }
 
-function ethGetBlockByNumber(call: Etherscan, tag: ProxyTag, boolean: boolean) {
+function getBlockByNumber(call: Etherscan, tag: ProxyTag, boolean: boolean) {
   if (boolean === true) {
     return call<BlockResponse<TransactionResponse[]>>({ module: 'proxy', action: 'eth_getBlockByNumber', tag, boolean });
   } else {
@@ -265,12 +339,12 @@ function ethGetBlockByNumber(call: Etherscan, tag: ProxyTag, boolean: boolean) {
   }
 }
 
-function ethGetUncleByBlockNumberAndIndex(call: Etherscan, tag: ProxyTag, params: Optional<UncleByBlockNumberAndIndexRequest>) {
+function getUncleByBlockNumberAndIndex(call: Etherscan, tag: ProxyTag, params: Optional<UncleByBlockNumberAndIndexRequest>) {
   return call<UncleBlockResponse>({ module: 'proxy', action: 'eth_getUncleByBlockNumberAndIndex', tag, ...params });
 }
 
-function getBlockTransactionCountByNumber(call: Etherscan, params: Optional<BlockTransactionCountByNumberRequest>) {
-  return call<string>({ module: 'proxy', action: 'eth_getBlockTransactionCountByNumber', ...params });
+async function getBlockTransactionCountByNumber(call: Etherscan, params: Optional<BlockTransactionCountByNumberRequest>) {
+  return await call<string>({ module: 'proxy', action: 'eth_getBlockTransactionCountByNumber', ...params });
 }
 
 function getTransactionByHash(call: Etherscan, txhash: string) {
