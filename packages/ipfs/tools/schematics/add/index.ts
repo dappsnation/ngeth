2ff9698fa96d0aa050431c8e38763ba6fa9423f2
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2022-07-20 08:57:18 +0200

    v0.0.23

--- packages/ipfs/tools/schematics/add/index.ts
+++ packages/ipfs/tools/schematics/add/index.ts
@@ -26,7 +26,7 @@ export async function nxGenerator(tree: Tree, baseOptions: BaseOptions) {
   updateGitIgnore(tree, '# IPFS Data\n.ipfs');
   
   const installTask = addDependenciesToPackageJson(tree, {
-    "@ngeth/ipfs": "0.0.22",
+    "@ngeth/ipfs": "0.0.23",
     "ipfs": "^0.62.0",
   }, {
     "ipfsd-ctl": "^10.0.6"
